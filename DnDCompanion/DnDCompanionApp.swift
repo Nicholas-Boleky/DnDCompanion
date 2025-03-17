9cd3de70a24fb66df41aaa24d7f8049f6bba773a
Nicholas Boleky <bolekyn@gmail.com>
2025-03-16 18:04:39 -0700

    Initial Commit

---
+++ DnDCompanion/DnDCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDCompanionApp.swift
+//  DnDCompanion
+//
+//  Created by Nick on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DnDCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DnDCompanionTests/DnDCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DnDCompanionTests.swift
+//  DnDCompanionTests
+//
+//  Created by Nick on 3/16/25.
+//
+
+import Testing
+@testable import DnDCompanion
+
+struct DnDCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
